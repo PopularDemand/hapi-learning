6a6c7804a18f4ef73d1b1aed97ccac341583ff40
Alexa Anderson <alexa.anderson@outlook.com>
2016-06-26 15:05:14 -0500

    Set up routes

--- hapi-tutorial/index.js
+++ hapi-tutorial/index.js
@@ -1,26 +1,45 @@
 var Hapi = require('hapi')
+// path is a node core module (no need to install)
+var Path = require('path')
 
 var server = new Hapi.Server()
 
-server.connection({
-  host: 'localhost',
-  port: 3000
-})
+server.connection({ port: 3000 })
 
-server.route({
-  method: 'GET',
-  path: '/',
-  handler: function(request, reply){
-    reply('hello')
-  }
-})
+// Note: vision require statement is HERE
+// Binding the vision plugin to the hapi server
+server.register(require('vision'), function(err) {
+  if (err) throw err
 
-server.route({
-  method: 'GET',
-  path: '/users/{userName}',
-  handler: function(request, reply) {
-    reply('Hello, ' + encodeURIComponent(request.params.userName))
-  }
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: function(request, reply){
+      // Go find the index view with rules from server.views
+      // view gets access data in the object
+      reply.view('index', {name: 'Alexa'})
+    }
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/users/{userName}',
+    handler: function(request, reply){
+      var name = encodeURIComponent(request.params.userName)
+      reply.view('user', {name: name})
+    }
+  })
+
+  // How the server finds and handles views
+  server.views({
+    engines: {
+      // handlebars require statement HERE
+      html: require('handlebars')
+    },
+    relativeTo: __dirname,
+    // In what path to look for templates (eg. __dirname/templates/[file].html)
+    path: 'templates'
+  })
 })
 
 server.start(function() {
